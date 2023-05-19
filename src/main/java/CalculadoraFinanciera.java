60875c0c8003be3b9522f96db83299a28481bdd3
daleivae <123129316+daleivae@users.noreply.github.com>
2023-05-19 18:01:05 -0400

    Correcciones de SonarQube

--- src/main/java/CalculadoraFinanciera.java
+++ src/main/java/CalculadoraFinanciera.java
@@ -10,13 +10,11 @@ public class CalculadoraFinanciera {
     }
     
     public double calcularAmortizacion(double capital, double tasa, int periodo) {
-        double cuota = capital * tasa / (1 - Math.pow((1 + tasa), -periodo));
-        return cuota;
+        return capital * tasa / (1 - Math.pow((1 + tasa), -periodo));
     }
     
     public double calcularTasaEfectiva(double tasaNominal, int frecuencia) {
-        double tasaEfectiva = Math.pow((1 + tasaNominal / frecuencia), frecuencia) - 1;
-        return tasaEfectiva;
+        return Math.pow((1 + tasaNominal / frecuencia), frecuencia) - 1;
     }
     
     // Otros métodos y funcionalidades de la calculadora financiera
